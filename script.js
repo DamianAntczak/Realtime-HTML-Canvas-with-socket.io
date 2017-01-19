59b487641cc20e354dd329e2e189218cb3d171e3
damiana <damian.antczak@questy.pl>
2017-01-19 20:21:54 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Damian on 19.01.2017.
+ */
+window.onload = function () {
+    console.log('Start');
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Damian on 19.01.2017.
+ */
+
+var app = require('http').createServer(handler)
+    ,fs = require('fs')
+    ,io = require('socket.io').listen(app);
+
+app.listen(8081);
+
+function handler(req, res) {
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+io.sockets.on('connection', function(socket) {
+    console.log('Polaczenie');
+
+    socket.on('draw', function(data) {
+        console.log('draw x - '+data.x+' y - '+data.y);
+        socket.broadcast.emit('draw', data);
+    });
+
+    socket.on('clear',function () {
+        console.log('clear');
+        socket.broadcast.emit('clear');
+    });
+});
